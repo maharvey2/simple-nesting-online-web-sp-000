35894687b08a86ded22ca567be5538b2bd7f9120
Mark Harvey <maharvey2@crimson.ua.edu>
2019-05-15 19:20:20 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -36,9 +36,7 @@ def alan_kay_is_known_for
           :languages => ["C"]
         }
      }
-
-		 alan_kay_is_known_for[:alan_kay]
-
+ alan_kay_is_known_for[:alan_kay][:known_for]
 end
 
 def dennis_ritchies_language
